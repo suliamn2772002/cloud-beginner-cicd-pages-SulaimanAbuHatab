279955461f85f114744e228f07aded4091c0c27b
suliamn2772002 <sul2772002@gmail.com>
2026-03-13 19:38:52 +0200

    Initial website commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", () => {
+  document.getElementById("msg").innerText =
+    "Hello! JavaScript is working successfully on my CI/CD website.";
+});
